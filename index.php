889402318abade8a33f7a16760fa7319714762bd
Gaurav <pantg835@gmail.com>
2020-06-28 22:36:09 +0530

    create feature & Database table has been created for demonstrating data records on the same page

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // INSERT INTO `notes` (`sno`, `tittle`, `description`, `tstamp`) VALUES ('1', 'alert', 'hello gaurav its php here', CURRENT_TIMESTAMP);
+$insert = false;
 //connect to database
 $servername = "localhost";
 $username = "root";
@@ -13,6 +14,22 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if(!$conn ){
    die("Could not connect: ". mysqli_connect_error());
 }
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+
+    //sql query to be executed
+    $sql = "INSERT INTO `notes` (`title`,`description`) VALUES ('$title', '$description')";
+
+    if (mysqli_query($conn, $sql)) {
+        $insert = true;
+     } else {
+        echo "Error: " . $sql . "" . mysqli_error($conn);
+     }
+
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -25,6 +42,8 @@ if(!$conn ){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">  
+      
 
     <title>curd Application</title>
 </head>
@@ -56,8 +75,20 @@ if(!$conn ){
             </form>
         </div>
     </nav>
+
+    <?php
+    if($insert){
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+        <strong>Success!</strong> You notes has been recorded successfully.
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>";
+    }
+    ?>
+
     <div class="container my-4">
-        <form>
+        <form action="/phpcurd/index.php" method="post" >
             <div class="form-group">
                 <label for="title">Note Tittle</label>
                 <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
@@ -65,15 +96,15 @@ if(!$conn ){
             </div>
             <div class="form-group">
                 <label for="desc">Note Description</label>
-                <textarea class="form-control" id="desc" name="desc" rows="3"></textarea>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
             </div>
 
             <button type="submit" class="btn btn-primary">Add Note</button>
         </form>
     </div>
-    <div class="container">
+    <div class="container my-4">
         
-        <table class="table">
+        <table class="table" id="myTable">
             <thead>
                 <tr>
                     <th scope="col">S.No</th>
@@ -89,7 +120,7 @@ if(!$conn ){
             while($row = mysqli_fetch_assoc($result)) {
                 echo "<tr>
                 <th scope='row'>". $row['sno'] . "</th>
-                <td>". $row['tittle'] . "</td>
+                <td>". $row['title'] . "</td>
                 <td>". $row['description'] ."</td>
                 <td>Tittle</td>
             </tr>";
@@ -102,6 +133,7 @@ if(!$conn ){
         </table>
 
     </div>
+    <hr>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
@@ -113,6 +145,12 @@ if(!$conn ){
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
         integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
         crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script>
+    $(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+    </script>
 </body>
 
 </html>
